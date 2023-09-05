ce582c96c1b35511166fc97c0761e2567c8300d4
Sergeyy321 <125265794+Sergeyy321@users.noreply.github.com>
2023-09-05 23:22:40 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,6 @@
-import { Statistics } from './statistics/Statistics';
+import { Statistics } from './statistics/statistics';
 import { Section } from './Section/Section';
-import {StatisticTitleError} from './statistics/Statistics.styled'
+import {StatisticTitleError} from './statistics/statistics.styled'
 import React, { Component } from 'react';
 import {Feedback} from './feedback/feedback'
 class App extends Component {
